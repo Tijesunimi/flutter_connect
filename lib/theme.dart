10dc12630c03e6208ce4a3806b02a9a096a7a7f9
mo <mojisolaadebiyi@yahoo.com>
2019-06-01 15:03:09 +0100

    Add Mentor'S Profile and Edit Profile

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme{
+  var headPrimaryColour = Color.fromRGBO(18, 21, 26, 1);
+  var writingPrimaryColour = Color.fromRGBO(147, 156, 182, 1);
+}
